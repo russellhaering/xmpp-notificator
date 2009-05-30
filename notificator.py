044bc4cb4478fd6b44cf943e61a1cce642bb5b3c
russell_h <russellhaering@gmail.com>
2009-05-30 13:26:33 -0700

    It doesn't do much yet, and seems to stop receiving messages after a while.

---
+++ notificator.py
@@ -0,0 +1,35 @@
+from twisted.internet import gtk2reactor
+gtk2reactor.install()
+from twisted.internet import reactor
+from twisted.words.protocols.jabber import jid
+from twisted.application import service
+from wokkel.xmppim import MessageProtocol, AvailablePresence
+from wokkel.client import XMPPClient
+from twisted.scripts import twistd
+import pynotify
+
+# Settings
+username = 'user'
+domain = 'jabber.org'
+password = 'somepass'
+
+
+class NotificationProtocol(MessageProtocol):
+    def connectionMade(self):
+        print "Connected!"
+        self.send(AvailablePresence())
+
+    def onMessage(self, msg):
+        # TODO: Clean up this 'if'
+        if msg["type"] == 'chat' and hasattr(msg, "body") and msg.body:
+            print str(msg.body)
+            notification = pynotify.Notification("Alert", str(msg.body))
+            notification.show()
+
+if __name__ == '__main__':
+    pynotify.init("XMPP Notificator")
+    xmppclient = XMPPClient(jid.internJID(username + "@" + domain + "/notificator"), password)
+    mynotificator = NotificationProtocol()
+    mynotificator.setHandlerParent(xmppclient)
+    xmppclient.startService()
+    reactor.run()
